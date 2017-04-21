2d6e3819b535fd8c8bda68b9e014c298b6a7c64f
James Shih <jamesshih.dev@gmail.com>
2017-04-21 10:18:48 +0800

    58. Adding data validation to RegisterMe app using REGEX

--- ch2-Swift 3 overview, Build more apps and Automate/RegisterMe/RegisterMe/ViewController.swift
+++ ch2-Swift 3 overview, Build more apps and Automate/RegisterMe/RegisterMe/ViewController.swift
@@ -17,23 +17,78 @@ class ViewController: UIViewController {
     
     var update_email = [String]()
     
+    func matches(for regex: String, in text: String) -> [String] {
+        
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let nsString = text as NSString
+            let results = regex.matches(in: text, range: NSRange(location: 0, length: nsString.length))
+            print(results.map { nsString.substring(with: $0.range)})
+            return results.map { nsString.substring(with: $0.range)}
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    func isNameValid() -> Bool {
+        
+        var matched = true
+        let pattern = "^[a-zA-Z\\s][a-zA-z0-9-_]{1,25}[a-zA-Z0-9}\\s+]$"
+        if matches(for: pattern, in: textField_name.text!).isEmpty {
+            matched = false
+        }
+        return matched
+    }
+    
+    func isEmailValid() -> Bool {
+        
+        var matched = true
+//        let pattern = "[a-zA-Z0-9.-_]+@[a-zA-Z0-9]+\\.[a-zA-Z]{1,4}"
+        let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        if matches(for: pattern, in: textField_email.text!).isEmpty {
+            matched = false
+        }
+        return matched
+    }
+    
+    func dropNameSpace() {
+        
+        let text = textField_name.text!
+        
+        if text.characters.first == " " {
+            textField_name.text = String(text.characters.dropFirst())
+        }
+        if text.characters.last == " " {
+            textField_name.text = String(text.characters.dropLast())
+        }
+    }
+    
     @IBAction func onSubmit(_ sender: Any) {
         
         if ((textField_name.text?.isEmpty)! && (textField_email.text?.isEmpty)!) {
             default_label.text = "Please enter a valid name and email address"
-            default_label.textColor = UIColor.red
         } else {
+            
             if(textField_name.text?.isEmpty)! {
-                default_label.text = "Please enter a valid name"
-                default_label.textColor = UIColor.red
+                default_label.text = "Please enter a valid name (code:1)"
             } else {
                 
-                if(textField_email.text?.isEmpty)! {
-                    default_label.text = "Please enter a valid email address"
-                    default_label.textColor = UIColor.red
+                if isNameValid() {
                     
+                    if(textField_email.text?.isEmpty)! {
+                        default_label.text = "Please enter a valid email address (code:1)"
+                    } else {
+                        if isEmailValid() {
+                            default_label.text = ""
+                            dropNameSpace()
+                        } else {
+                            default_label.text = "Please enter a valid email address (code:2)"
+                        }
+                        
+                    }
                 } else {
-                    default_label.text = ""
+                    default_label.text = "Please enter a valid name (code:2)"
                 }
             }
         }
